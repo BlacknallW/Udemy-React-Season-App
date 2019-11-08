3f02e1258b165c4a5efba867b7aa3fe178e8a1da
William Blacknall <williamblacknall@gmail.com>
2019-11-08 13:41:56 -0500

    Added styling to pages variant as to which season it is, beginning process to add loading spinner thing

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import {Icon} from 'semantic-ui-react'
+import "./SeasonDisplay.css"
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr it's cold!",
+        iconName: 'snowflake'
+    },
+}
 
 const getSeason = (lat,month) => {
     if(month > 2 && month < 9){
@@ -11,13 +21,11 @@ const getSeason = (lat,month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth())
-    const text = season === "winter" ? "Burr it's chilly!" : "Let's hit the beach!"
-    const icon = season === "winter" ? 'snowflake' : 'sun';
-    return (
-    <div>
-        <i className={`${icon} icon`}/>
+    const {text, iconName} = seasonConfig[season]; 
+    return(<div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}/>
         <h1>{text}</h1>
-        <i className={`${icon} icon`}/>
+        <i className={`icon-right massive ${iconName} icon`}/>
     </div>
     )
 };
